dbf5857b52a1d016414f24e69f9e9eca3bcd674e
Niccar95 <niccar.95@outlook.com>
2024-02-17 15:14:17 +0100

    simple to do list created

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const listSection = document.getElementById("listSection");
+const listForm = document.querySelector(".listForm");
+const taskInput = document.querySelector(".taskInput") as HTMLInputElement;
+const submitButton = document.getElementById("submitButton");
+const taskSection = document.getElementById("taskSection");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const taskList: string[] = [];
+
+
+listForm?.addEventListener("submit", (e) => {
+e.preventDefault();
+
+  const taskInputValue = taskInput?.value;
+
+  if (taskInputValue) { 
+    taskList.push(taskInputValue);
+    console.log(taskList);
+  }
+
+    const newList = document.querySelector("ul");
+    const listItem = document.createElement("li");
+
+    newList?.appendChild(listItem);
+    
+    listItem.innerHTML = taskInputValue;
+
+    taskInput.value = "";
+
+});
